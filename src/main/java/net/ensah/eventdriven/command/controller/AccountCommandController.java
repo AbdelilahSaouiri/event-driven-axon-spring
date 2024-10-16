71a42014ba2255d71c2ba34e68244df894309e6f
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2024-10-16 23:54:30 +0100

    update

--- src/main/java/net/ensah/eventdriven/command/controller/AccountCommandController.java
+++ src/main/java/net/ensah/eventdriven/command/controller/AccountCommandController.java
@@ -3,22 +3,24 @@ package net.ensah.eventdriven.command.controller;
 import net.ensah.eventdriven.commun.commands.CreateAccountCommand;
 import net.ensah.eventdriven.commun.dtos.CreateAccountRequestDto;
 import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.axonframework.eventhandling.DomainEventMessage;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/commands/v1/accounts")
 public class AccountCommandController {
 
     private final CommandGateway commandGateway;
+    private final EventStore eventStore;
 
-    public AccountCommandController(CommandGateway cmdGateway) {
+    public AccountCommandController(CommandGateway cmdGateway, EventStore eventStore) {
         this.commandGateway = cmdGateway;
+        this.eventStore = eventStore;
     }
 
 
@@ -29,6 +31,11 @@ public class AccountCommandController {
                 requestDto.initialBalance(),
                 requestDto.currency()
         ));
+    }
+
+    @GetMapping("/{accountId}")
+    public Stream<?> eventStore(@PathVariable String accountId){
+        return eventStore.readEvents(accountId).asStream();
     }
 
 }
