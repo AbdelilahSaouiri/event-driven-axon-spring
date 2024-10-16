abbf63f2c3899695d80d7fd9b6978b11be9bbcd0
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2024-10-16 18:48:24 +0100

    update one

--- src/main/java/net/ensah/eventdriven/command/controller/AccountCommandController.java
+++ src/main/java/net/ensah/eventdriven/command/controller/AccountCommandController.java
@@ -1,19 +1,34 @@
 package net.ensah.eventdriven.command.controller;
 
+import net.ensah.eventdriven.commun.commands.CreateAccountCommand;
 import net.ensah.eventdriven.commun.dtos.CreateAccountRequestDto;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/commands/v1/accounts")
 public class AccountCommandController {
 
+    private final CommandGateway commandGateway;
+
+    public AccountCommandController(CommandGateway cmdGateway) {
+        this.commandGateway = cmdGateway;
+    }
 
 
     @PostMapping
-    public void CreateNewAccount(CreateAccountRequestDto requestDto){
-
+    public  CompletableFuture<String> CreateNewAccount(@RequestBody CreateAccountRequestDto requestDto){
+        return commandGateway.send(new CreateAccountCommand<>(
+                UUID.randomUUID().toString(),
+                requestDto.initialBalance(),
+                requestDto.currency()
+        ));
     }
 
 }
